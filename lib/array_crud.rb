0ad597b27e2a7006c447221b22f9320a3e11d1b7
Carter Parrett <cparrett94@hotmail.com>
2020-07-08 02:46:29 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -27,7 +27,7 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
-  league_of_legends = ["wow", "Nasus", "Sion", "arrays!"]
+  league_of_legends = ["wow", "am", "Sion", "arrays!"]
   league_of_legends[1]
 end
 
