cac08b331ab182de119f6191fe25244a5420ca45
Carter Parrett <cparrett94@hotmail.com>
2020-07-08 02:43:14 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -17,7 +17,7 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
-  league_of_legends = ["Sona", "Nasus", "Sion", "Fizz"]
+  league_of_legends = ["Sona", "Nasus", "Sion", "arrays!"]
   league_of_legends.pop
 end
 
