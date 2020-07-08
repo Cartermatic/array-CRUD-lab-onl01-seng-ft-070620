c35f597c3eb5b11b4bd2bc539a40bef8db11ced1
Carter Parrett <cparrett94@hotmail.com>
2020-07-08 02:46:04 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -28,7 +28,7 @@ end
 
 def retrieve_element_from_index(array, index_number)
   league_of_legends = ["wow", "Nasus", "Sion", "arrays!"]
-  league_of_legends
+  league_of_legends[1]
 end
 
 def retrieve_first_element_from_array(array)
